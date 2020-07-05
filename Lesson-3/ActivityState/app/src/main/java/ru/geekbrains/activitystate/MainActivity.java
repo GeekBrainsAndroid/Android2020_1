34de24de514e92d313d1b366f70b60a334fee65c
DwarfPorter <mrvlmor@gmail.com>
2020-07-05 16:36:12 +0300

    ActivityState: Обработка нажатий - первый способ, через onClick в макете

--- Lesson-3/ActivityState/app/src/main/java/ru/geekbrains/activitystate/MainActivity.java
+++ Lesson-3/ActivityState/app/src/main/java/ru/geekbrains/activitystate/MainActivity.java
@@ -3,12 +3,30 @@ package ru.geekbrains.activitystate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int counter1 = 0;       // первый счетчик
+    private TextView textCounter1;  // пользовательский элемент 1-го счетчика
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Получить пользовательский элемент по идентификатору
+        textCounter1 = findViewById(R.id.textView1);
+    }
+
+    // Обработка кнопки через атрибут onClick в макете
+    public void button1_onClick(View view) {
+        counter1++;
+
+        // Установить текст на TextView
+        textCounter1.setText(String.format(Locale.getDefault(), "%d", counter1));
     }
 }
