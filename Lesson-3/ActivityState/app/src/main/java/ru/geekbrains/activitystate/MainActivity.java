4c4a65cf2d1c79e4b95a8f88bfb7377b125f0674
DwarfPorter <mrvlmor@gmail.com>
2020-07-05 16:36:45 +0300

    ActivityState: Обработка нажатий - способ слушателя

--- Lesson-3/ActivityState/app/src/main/java/ru/geekbrains/activitystate/MainActivity.java
+++ Lesson-3/ActivityState/app/src/main/java/ru/geekbrains/activitystate/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Locale;
@@ -11,15 +12,29 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     private int counter1 = 0;       // первый счетчик
+    private int counter2 = 0;       // Второй счетчик
+
     private TextView textCounter1;  // пользовательский элемент 1-го счетчика
+    private TextView textCounter2;  // пользовательский элемент 1-го счетчика
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Получить пользовательский элемент по идентификатору
+        // Получить пользовательские элементы по идентификатору
         textCounter1 = findViewById(R.id.textView1);
+        textCounter2 = findViewById(R.id.textView2);
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter2++;
+
+                //установить текст на TextView
+                textCounter2.setText(String.format(Locale.getDefault(), "%d", counter2));
+            }
+        });
     }
 
     // Обработка кнопки через атрибут onClick в макете
