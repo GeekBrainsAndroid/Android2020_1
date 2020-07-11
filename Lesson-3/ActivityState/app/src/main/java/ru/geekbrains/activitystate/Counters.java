7cc360dcb368852fcd183b3746e5ffe97bb3c5f0
DwarfPorter <mrvlmor@gmail.com>
2020-07-11 21:58:49 +0300

    ActivityState: Обработка нажатий - активити как слушатель и слушатель-поле

--- Lesson-3/ActivityState/app/src/main/java/ru/geekbrains/activitystate/Counters.java
+++ Lesson-3/ActivityState/app/src/main/java/ru/geekbrains/activitystate/Counters.java
@@ -3,10 +3,14 @@ package ru.geekbrains.activitystate;
 public class Counters {
     private int counter1;
     private int counter2;
+    private int counter3;
+    private int counter4;
 
     public Counters(){
         counter1 = 0;
         counter2 = 0;
+        counter3 = 0;
+        counter4 = 0;
     }
 
     public int getCounter1() {
@@ -17,11 +21,27 @@ public class Counters {
         return counter2;
     }
 
+    public int getCounter3() {
+        return counter3;
+    }
+
+    public int getCounter4() {
+        return counter4;
+    }
+
     public void incrementCounter1(){
         counter1++;
     }
 
     public void incrementCounter2(){
         counter2++;
+    }
+
+    public void incrementCounter3(){
+        counter3++;
+    }
+
+    public void incrementCounter4(){
+        counter4++;
     }
 }
