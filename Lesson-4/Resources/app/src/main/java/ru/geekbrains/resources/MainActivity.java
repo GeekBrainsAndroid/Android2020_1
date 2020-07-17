b94bb9b4226fce1f04f2945e0a9c663c1fd8b165
DwarfPorter <mrvlmor@gmail.com>
2020-07-17 23:56:28 +0300

    Resources: Работа с Assets

--- Lesson-4/Resources/app/src/main/java/ru/geekbrains/resources/MainActivity.java
+++ Lesson-4/Resources/app/src/main/java/ru/geekbrains/resources/MainActivity.java
@@ -1,9 +1,16 @@
 package ru.geekbrains.resources;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +19,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/19659.ttf");
+
         // Вывод ресурсов в строки программно
         TextView descriptionLanguage = findViewById(R.id.textVLang);
         descriptionLanguage.setText(getResources().getString(R.string.descriptionLanguage));
+        descriptionLanguage.setTypeface(tf);
         TextView textLanguage = findViewById(R.id.textLang);
         textLanguage.setText(getResources().getString(R.string.language));
+
+        AppCompatImageView image = findViewById(R.id.imageView);
+        loadImageFromAsset(image, "android.png");
+    }
+
+    public void loadImageFromAsset(ImageView image, String fileName) {
+        try {
+            InputStream ims = getAssets().open(fileName);
+            // загружаем как Drawable
+            Drawable d = Drawable.createFromStream(ims, null);
+            // выводим картинку в ImageView
+            image.setImageDrawable(d);
+        }
+        catch(IOException ex) {
+            return;
+        }
     }
 }
