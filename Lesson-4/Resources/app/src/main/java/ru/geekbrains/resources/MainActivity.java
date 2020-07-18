01e21267245e9eacf9fbcdfcbb67f1e8ba74d087
DwarfPorter <mrvlmor@gmail.com>
2020-07-18 09:29:32 +0300

    Resources: Работа с массивами

--- Lesson-4/Resources/app/src/main/java/ru/geekbrains/resources/MainActivity.java
+++ Lesson-4/Resources/app/src/main/java/ru/geekbrains/resources/MainActivity.java
@@ -3,10 +3,14 @@ package ru.geekbrains.resources;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 
+import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -19,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initSomeTexts();
+
+        AppCompatImageView image = findViewById(R.id.imageView);
+        loadImageFromAsset(image, "android.png");
+
+        initList();
+    }
+
+    private void initSomeTexts() {
         Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/19659.ttf");
 
         // Вывод ресурсов в строки программно
@@ -27,12 +40,9 @@ public class MainActivity extends AppCompatActivity {
         descriptionLanguage.setTypeface(tf);
         TextView textLanguage = findViewById(R.id.textLang);
         textLanguage.setText(getResources().getString(R.string.language));
-
-        AppCompatImageView image = findViewById(R.id.imageView);
-        loadImageFromAsset(image, "android.png");
     }
 
-    public void loadImageFromAsset(ImageView image, String fileName) {
+    private void loadImageFromAsset(ImageView image, String fileName) {
         try {
             InputStream ims = getAssets().open(fileName);
             // загружаем как Drawable
@@ -42,6 +52,34 @@ public class MainActivity extends AppCompatActivity {
         }
         catch(IOException ex) {
             return;
+        }
+    }
+
+    private void initList(){
+        LinearLayout layoutList = findViewById(R.id.layoutList);
+
+        // Получить массив со строками из ресурсов
+        String[] versions = getResources().getStringArray(R.array.version_names);
+
+        // Получить из ресурсов массив указателей на изображения
+        TypedArray imgs = getResources().obtainTypedArray(R.array.version_logos);
+
+        // При помощи этого объекта будем доставать элементы, спрятанные в android_item.xml
+        LayoutInflater ltInflater = getLayoutInflater();
+
+        for (int i = 0; i < versions.length; i++){
+            String version = versions[i];
+            // Достаем элемент из item.xml
+            View item = ltInflater.inflate(R.layout.android_item, layoutList, false);
+            // Находим в этом элементе TextView
+            TextView tv = item.findViewById(R.id.textAndroid);
+            tv.setText(version);
+
+            // Выбрать по индексу подходящее изображение
+            AppCompatImageView imgLogo = item.findViewById(R.id.imageAndroid);
+            imgLogo.setImageResource(imgs.getResourceId(i, -1));
+
+            layoutList.addView(item);
         }
     }
 }
