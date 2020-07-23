10642f3839597fea2e8666b1f90724bb786628e5
DwarfPorter <mrvlmor@gmail.com>
2020-07-23 16:47:07 +0300

    Account: 1-я активити

--- Lesson-5/Account/app/src/main/java/ru/geekbrains/account/MainActivity.java
+++ Lesson-5/Account/app/src/main/java/ru/geekbrains/account/MainActivity.java
@@ -1,14 +1,33 @@
 package ru.geekbrains.account;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        Button btnGreetings = findViewById(R.id.btnGreetings);
+        final EditText txtName = findViewById(R.id.textName);
+        final TextView txtGreetings = findViewById(R.id.textHello);
+        btnGreetings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = txtName.getText().toString();
+                String sayHello = getString(R.string.say_hello) + name;
+                txtGreetings.setText(sayHello);
+            }
+        });
     }
 }
