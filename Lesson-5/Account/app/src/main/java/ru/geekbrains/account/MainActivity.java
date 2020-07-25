a99880853bdb0b52a485d808fc4d3e9b0be533ea
DwarfPorter <mrvlmor@gmail.com>
2020-07-25 16:02:01 +0300

    Account: 2-я активити, настройки.

--- Lesson-5/Account/app/src/main/java/ru/geekbrains/account/MainActivity.java
+++ Lesson-5/Account/app/src/main/java/ru/geekbrains/account/MainActivity.java
@@ -1,12 +1,13 @@
 package ru.geekbrains.account;
 
-        import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.TextView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -27,6 +28,18 @@ public class MainActivity extends AppCompatActivity {
                 String name = txtName.getText().toString();
                 String sayHello = getString(R.string.say_hello) + name;
                 txtGreetings.setText(sayHello);
+            }
+        });
+
+        Button btnSettings = findViewById(R.id.btnSettings);
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Чтобы стартовать активити надо подготовить интент
+                // В данном случае это будет явный интент, поскольку здесь передается класс активити
+                Intent runSettings = new Intent(MainActivity.this, SettingsActivity.class);
+                // Метод стратует активити, указанную в интенте
+                startActivity(runSettings);
             }
         });
     }
---
+++ Lesson-5/Account/app/src/main/java/ru/geekbrains/account/SettingsActivity.java
@@ -0,0 +1,25 @@
+package ru.geekbrains.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Button btnReturn = findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Метод finish() завершает активити
+                finish();
+            }
+        });
+    }
+}
