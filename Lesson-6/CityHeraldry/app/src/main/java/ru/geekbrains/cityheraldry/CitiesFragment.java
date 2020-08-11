424c184a27012ec9a8a822369f556f9dcced6491
DwarfPorter <mrvlmor@gmail.com>
2020-08-11 09:04:13 +0300

    CityHeraldry: Создание списка городов

---
+++ Lesson-6/CityHeraldry/app/src/main/java/ru/geekbrains/cityheraldry/CitiesFragment.java
@@ -0,0 +1,47 @@
+package ru.geekbrains.cityheraldry;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CitiesFragment extends Fragment {
+
+    // При создании фрагмента укажем его макет
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_cities, container, false);
+    }
+
+    // вызывается после создания макета фрагмента, здесь мы проинициализируем список
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initList(view);
+    }
+
+    // создаем список городов на экране из массива в ресурсах
+    private void initList(View view) {
+        LinearLayout layoutView = (LinearLayout)view;
+        String[] cities = getResources().getStringArray(R.array.cities);
+
+        // В этом цикле создаем элемент TextView,
+        // заполняем его значениями,
+        // и добавляем на экран.
+        // Кроме того, создаем обработку касания на элемент
+        for(int i=0; i < cities.length; i++){
+            String city = cities[i];
+            TextView tv = new TextView(getContext());
+            tv.setText(city);
+            tv.setTextSize(30);
+            layoutView.addView(tv);
+        }
+    }
+}
