84b45145e7297c24a212185b40bee1e06a8226b5
DwarfPorter <mrvlmor@gmail.com>
2020-08-11 12:12:05 +0300

    CityHeraldry: Показ герба в ландшафтной ориентации

--- Lesson-6/CityHeraldry/app/src/main/java/ru/geekbrains/cityheraldry/CitiesFragment.java
+++ Lesson-6/CityHeraldry/app/src/main/java/ru/geekbrains/cityheraldry/CitiesFragment.java
@@ -1,6 +1,7 @@
 package ru.geekbrains.cityheraldry;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +12,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class CitiesFragment extends Fragment {
+
+    private boolean isLandscape;
 
     // При создании фрагмента укажем его макет
     @Override
@@ -30,14 +35,14 @@ public class CitiesFragment extends Fragment {
 
     // создаем список городов на экране из массива в ресурсах
     private void initList(View view) {
-        LinearLayout layoutView = (LinearLayout)view;
+        LinearLayout layoutView = (LinearLayout) view;
         String[] cities = getResources().getStringArray(R.array.cities);
 
         // В этом цикле создаем элемент TextView,
         // заполняем его значениями,
         // и добавляем на экран.
         // Кроме того, создаем обработку касания на элемент
-        for(int i=0; i < cities.length; i++){
+        for (int i = 0; i < cities.length; i++) {
             String city = cities[i];
             TextView tv = new TextView(getContext());
             tv.setText(city);
@@ -47,19 +52,55 @@ public class CitiesFragment extends Fragment {
             tv.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    showPortCoatOfArms(fi);
+                    showCoatOfArms(fi);
                 }
             });
         }
     }
 
+    // activity создана, можно к ней обращаться. Выполним начальные действия
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Определение, можно ли будет расположить рядом герб в другом фрагменте
+        isLandscape = getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE;
+
+        // Если можно нарисовать рядом герб, то сделаем это
+        if (isLandscape) {
+            showLandCoatOfArms(0);
+        }
+    }
+
+    private void showCoatOfArms(int index) {
+        if (isLandscape) {
+            showLandCoatOfArms(index);
+        } else {
+            showPortCoatOfArms(index);
+        }
+    }
+
+    // Показать герб в ландшафтной ориентации
+    private void showLandCoatOfArms(int index) {
+        // Создаем новый фрагмент с текущей позицией для вывода герба
+        CoatOfArmsFragment detail = CoatOfArmsFragment.newInstance(index);
+
+        // Выполняем транзакцию по замене фрагмента
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.coat_of_arms, detail);  // замена фрагмента
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
+    }
+
     // Показать герб в портретной ориентации.
     private void showPortCoatOfArms(int index) {
-            // Откроем вторую activity
-            Intent intent = new Intent();
-            intent.setClass(getActivity(), CoatOfArmsActivity.class);
-            // и передадим туда параметры
-            intent.putExtra(CoatOfArmsFragment.ARG_INDEX, index);
-            startActivity(intent);
+        // Откроем вторую activity
+        Intent intent = new Intent();
+        intent.setClass(getActivity(), CoatOfArmsActivity.class);
+        // и передадим туда параметры
+        intent.putExtra(CoatOfArmsFragment.ARG_INDEX, index);
+        startActivity(intent);
     }
 }
