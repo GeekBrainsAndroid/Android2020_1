477c91810cc05c9f3bca2bb25caeca2918979f52
DwarfPorter <mrvlmor@gmail.com>
2020-08-13 17:43:18 +0300

    Debug: Исправлено условие

--- Lesson-1/Debug/app/src/main/java/ru/geekbrains/debug/MainActivity.java
+++ Lesson-1/Debug/app/src/main/java/ru/geekbrains/debug/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 arg1 = Integer.valueOf(editText1.getText().toString());
                 arg2 = Integer.valueOf(editText2.getText().toString());
-                if (arg1 == arg2) {
+                if (arg1.equals(arg2)) {
                     textView.setText("Равно!");
                 } else {
                     textView.setText("Не равно!");
