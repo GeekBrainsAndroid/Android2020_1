09dde1523ae2634332e9b221f12568300e8507d4
DwarfPorter <mrvlmor@gmail.com>
2020-08-23 17:46:39 +0300

    FragmentNavigation: Создание popup меню

--- Lesson-7/FragmentNavigation/app/src/main/java/ru/geekbrains/fragmentnavigation/MainFragment.java
+++ Lesson-7/FragmentNavigation/app/src/main/java/ru/geekbrains/fragmentnavigation/MainFragment.java
@@ -1,5 +1,7 @@
 package ru.geekbrains.fragmentnavigation;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -7,10 +9,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
 
 public class MainFragment extends Fragment {
@@ -20,7 +24,42 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         setHasOptionsMenu(true);
+        initPopupMenu(view);
         return view;
+    }
+
+    private void initPopupMenu(View view) {
+        TextView text = view.findViewById(R.id.textView);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Activity activity = requireActivity();
+                PopupMenu popupMenu = new PopupMenu(activity, v);
+                activity.getMenuInflater().inflate(R.menu.popup, popupMenu.getMenu());
+                Menu menu = popupMenu.getMenu();
+                menu.findItem(R.id.item2_popup).setVisible(false);
+                menu.add(0, 123456, 12, R.string.new_menu_item_added);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        int id = item.getItemId();
+                        switch (id) {
+                            case R.id.item1_popup:
+                                Toast.makeText(getContext(), "Chosen popup item 1", Toast.LENGTH_SHORT).show();
+                                return true;
+                            case R.id.item2_popup:
+                                Toast.makeText(getContext(), "Chosen popup item 2", Toast.LENGTH_SHORT).show();
+                                return true;
+                            case 123456:
+                                Toast.makeText(getContext(), "Chosen new item added", Toast.LENGTH_SHORT).show();
+                                return true;
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
     }
 
     @Override
@@ -36,5 +75,4 @@ public class MainFragment extends Fragment {
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
