3d0154fbf208dbcbc86f61f594cabad40194df85
DwarfPorter <mrvlmor@gmail.com>
2020-08-23 10:49:54 +0300

    FragmentNavigation: Добавление главного меню

--- Lesson-7/FragmentNavigation/app/src/main/java/ru/geekbrains/fragmentnavigation/MainActivity.java
+++ Lesson-7/FragmentNavigation/app/src/main/java/ru/geekbrains/fragmentnavigation/MainActivity.java
@@ -1,14 +1,19 @@
 package ru.geekbrains.fragmentnavigation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -23,10 +28,58 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        initToolbar();
         initButtonMain();
         initButtonFavorite();
         initButtonSettings();
         initButtonBack();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Обработка выбора пункта меню приложения (активити)
+        int id = item.getItemId();
+
+        switch(id){
+            case R.id.action_settings:
+                addFragment(new SettingsFragment());
+                return true;
+            case R.id.action_main:
+                addFragment(new MainFragment());
+                return true;
+            case R.id.action_favorite:
+                addFragment(new FavoriteFragment());
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Здесь определяем меню приложения (активити)
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem search = menu.findItem(R.id.action_search); // поиск пункта меню поиска
+        SearchView searchText = (SearchView) search.getActionView(); // строка поиска
+        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            // реагирует на конец ввода поиска
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            // реагирует на нажатие каждой клавиши
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+
+        return true;
     }
 
     private void initButtonBack() {
