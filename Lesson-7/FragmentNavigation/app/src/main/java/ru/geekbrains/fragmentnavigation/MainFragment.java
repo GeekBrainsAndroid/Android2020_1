bd4592e4f76c416e7152d794befaf08e7efe6249
DwarfPorter <mrvlmor@gmail.com>
2020-08-23 11:36:29 +0300

    FragmentNavigation: Добавление пунктов меню для фрагмента

--- Lesson-7/FragmentNavigation/app/src/main/java/ru/geekbrains/fragmentnavigation/MainFragment.java
+++ Lesson-7/FragmentNavigation/app/src/main/java/ru/geekbrains/fragmentnavigation/MainFragment.java
@@ -2,9 +2,14 @@ package ru.geekbrains.fragmentnavigation;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -14,6 +19,22 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+        setHasOptionsMenu(true);
         return view;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.main_fragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_add){
+            Toast.makeText(getContext(), "Chosen add", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
